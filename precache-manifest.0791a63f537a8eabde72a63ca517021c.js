c4ffaeae1fba15be355ed38166cc2a9b316efef4
malegovainna <102613046+malegovainna@users.noreply.github.com>
2022-05-12 21:59:33 +0300

    Add files via upload

---
+++ precache-manifest.0791a63f537a8eabde72a63ca517021c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5546d5caad3875b2de5eb558ba9fec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6eeb5511b7059deb9c3",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "b6eeb5511b7059deb9c3",
+    "url": "/static/js/main.dae1e251.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
